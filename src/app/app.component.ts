77a6822dc299afcf890c77e0245d6cabfa38f168
Daniel Slvka <visuateca@gmail.com>
2020-09-30 10:17:29 +0200

    Initially generated code cleared

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mars-rover-photos';
+
+  title = 'Mars Rover Photos';
+
 }
