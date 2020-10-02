d87be01a6b9f95a5e85fa9ae364acca97646d754
Daniel Slvka <visuateca@gmail.com>
2020-10-02 04:56:20 +0200

    Environment paths update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiDataPath: "https://api.nasa.gov/mars-photos/api/v1/rovers",
+  apiPhotosPath: "https://api.nasa.gov/mars-photos/api/v1/rovers",
 	apiManifestPath: "https://api.nasa.gov/mars-photos/api/v1/manifests",
 	apiKey: "DEMO_KEY"
 };
