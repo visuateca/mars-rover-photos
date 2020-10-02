08cc3581c677c7fadcd63901369396d4074f7e11
Daniel Slvka <visuateca@gmail.com>
2020-10-02 11:33:48 +0200

    Home module initial impl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HomeModule,
     AppRoutingModule
   ],
   providers: [],
